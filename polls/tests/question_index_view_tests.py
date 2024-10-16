85f7279a597bb403529e8ecbe9ceae4e9bd99572
Taichi Iida <chiaki.621@gmail.com>
2024-10-16 10:45:48 +0900

    テスト修正

--- polls/tests/question_index_view_tests.py
+++ polls/tests/question_index_view_tests.py
@@ -3,6 +3,19 @@ from django.test import TestCase
 # Create your tests here.
 
 from django.urls import reverse
+import datetime
+from django.utils import timezone
+from ..models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
