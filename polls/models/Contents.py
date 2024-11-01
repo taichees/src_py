1cf714b070057b259c233bdc3d0236e6e43d1d9e
malfoy33 <chishimaru12@icloud.com>
2024-11-01 22:22:44 +0900

    細部の調整

--- polls/models/Contents.py
+++ polls/models/Contents.py
@@ -6,4 +6,4 @@ class Contents(models.Model):
     contentId = models.AutoField(primary_key=True)  # 自動でインクリメントされる整数の主キー
     content = models.TextField()  # テキスト型
     userId = models.ForeignKey(Users, on_delete=models.CASCADE)  # Userテーブルの外部キー
-    date = models.DateTimeField()  # タイムスタンプ型
+    date = models.DateTimeField(auto_now=True)  # タイムスタンプ型
