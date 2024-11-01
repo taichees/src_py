748a7d574537c909aff884853fd5d768dcc55be8
malfoy33 <chishimaru12@icloud.com>
2024-11-01 22:07:30 +0900

    contentsテーブルのカラム変更

--- polls/models/Contents.py
+++ polls/models/Contents.py
@@ -3,10 +3,7 @@ from django.db import models
 from . import Users
 
 class Contents(models.Model):
-    contentsId = models.AutoField(primary_key=True)  # 自動でインクリメントされる整数の主キー
+    contentId = models.AutoField(primary_key=True)  # 自動でインクリメントされる整数の主キー
     content = models.TextField()  # テキスト型
     userId = models.ForeignKey(Users, on_delete=models.CASCADE)  # Userテーブルの外部キー
-    data = models.TextField()  # テキスト型
-
-    # def __str__(self):
-    #     return self.content
+    date = models.DateTimeField()  # タイムスタンプ型
--- polls/models/Users.py
+++ polls/models/Users.py
@@ -4,6 +4,3 @@ class Users(models.Model):
     userId = models.AutoField(primary_key=True)  # 自動でインクリメントされる整数の主キー
     userName = models.TextField()  # テキスト型
     password = models.TextField()  # テキスト型
-
-    # def __str__(self):
-    #     return self.userName
